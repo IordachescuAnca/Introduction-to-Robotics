3a189a4903284accf07b96be77c375ef0c074479
Iordachescu Anca <iordachescu.ancamihaela@yahoo.ro>
2019-11-22 17:31:07 +0200

    Update 4digitsSevenSegmentDisplay.ino

--- Lab-Homework/4digitsSevenSegmentDisplay.ino
+++ Lab-Homework/4digitsSevenSegmentDisplay.ino
@@ -1,3 +1,6 @@
+const uint8_t xPin = A0;
+const uint8_t yPin = A1;
+const uint8_t joyButton_pushPin = 1;
 const int pinA = 12;
 const int pinB = 8;
 const int pinC = 5;
@@ -5,65 +8,58 @@ const int pinD = 3;
 const int pinE = 2;
 const int pinF = 11;
 const int pinG = 6;
-
 const int pinDP = 4;
-
 const int pinD1 = 7;
 const int pinD2 = 9;
 const int pinD3 = 10;
 const int pinD4 = 13;
 
-const int pinSW = 1;
-const int pinX = A0;
-const int pinY = A1;
 
 const int segSize = 8;
-int segments[segSize] = {pinA, pinB, pinC, pinD, pinE, pinF, pinG, pinDP};
-
 const int noOfDisplays = 4;
-int digits[noOfDisplays] = {pinD1, pinD2, pinD3, pinD4};
-
-const int hash[noOfDisplays+1] = {-1, 3, 1, 2, 0};
-const int BLINK_SPEED = 500;
-
 const int noOfDigits = 10;
+
+int segments[segSize] = { pinA, pinB, pinC, pinD, pinE, pinF, pinG, pinDP };
+int digits[noOfDisplays] = { pinD1, pinD2, pinD3, pinD4 };
+
 byte digitMatrix[noOfDigits][segSize - 1] = {
-// a b c d e f g
-{1, 1, 1, 1, 1, 1, 0}, // 0
-{0, 1, 1, 0, 0, 0, 0}, // 1
-{1, 1, 0, 1, 1, 0, 1}, // 2
-{1, 1, 1, 1, 0, 0, 1}, // 3
-{0, 1, 1, 0, 0, 1, 1}, // 4
-{1, 0, 1, 1, 0, 1, 1}, // 5
-{1, 0, 1, 1, 1, 1, 1}, // 6
-{1, 1, 1, 0, 0, 0, 0}, // 7
-{1, 1, 1, 1, 1, 1, 1}, // 8
-{1, 1, 1, 1, 0, 1, 1} // 9
+//   a  b  c  d  e  f  g
+  {1, 1, 1, 1, 1, 1, 0}, // 0
+  {0, 1, 1, 0, 0, 0, 0}, // 1
+  {1, 1, 0, 1, 1, 0, 1}, // 2
+  {1, 1, 1, 1, 0, 0, 1}, // 3
+  {0, 1, 1, 0, 0, 1, 1}, // 4
+  {1, 0, 1, 1, 0, 1, 1}, // 5
+  {1, 0, 1, 1, 1, 1, 1}, // 6
+  {1, 1, 1, 0, 0, 0, 0}, // 7
+  {1, 1, 1, 1, 1, 1, 1}, // 8
+  {1, 1, 1, 1, 0, 1, 1}  // 9
 };
 
 int xValue = 0;
 int yValue = 0;
-int swValue = 0;
-int lastSwValue = 0;
 
-const int minValueY = 300;
-const int maxValueY = 700;
-int contorY = 1;
-int joyMovedY = 0;
+int minLimitValue = 350;
+int maxLimitValue = 700;
+bool pushButtonValue;
 
-const int minValueX = 300;
-const int maxValueX = 700;
-int contorX = 0;
-int joyMovedX = 0;
+bool isMoved = 0;
+bool dpState = 0;
 
-int isBlocked = 0;
+int joyButton_lastDebounce = 0;
+bool joyButton_lastPushValue;
+int joyButton_pushDebounceInterval = 20;
 
-void displayNumber(byte digit, byte decimalPoint) {
-  for (int i = 0; i < segSize-1; i++) {
-    digitalWrite(segments[i], digitMatrix[digit][i]);
-  }
-  digitalWrite(segments[segSize - 1], decimalPoint);
-}
+int currentDigitsValues[noOfDisplays] = { 0, 0, 0, 0 };
+
+int currentDigit = 0;
+int selectedDigit = noOfDisplays;
+int digitToEdit = noOfDisplays - 1;
+
+
+int flushInterval = 1;
+int lastFlush = 0;
+
 
 void showDigit(int num) {
   for (int i = 0; i < noOfDisplays; i++) {
@@ -72,86 +68,127 @@ void showDigit(int num) {
   digitalWrite(digits[num], LOW);
 }
 
+void displayDigit(int num, byte digit, byte decimalPoint) {
+  showDigit(num);
+  for (int i = 0; i < segSize - 1; i++) {
+    digitalWrite(segments[i], digitMatrix[digit][i]);
+  }
+  digitalWrite(segments[segSize - 1], decimalPoint);
+}
 
-void setup(){
-   pinMode(pinSW, INPUT_PULLUP);
-   for (int i = 0; i < segSize; i++)
-  {
+
+
+void setup() {
+  for (uint8_t i = 0; i < segSize; i++) {
     pinMode(segments[i], OUTPUT);
   }
-  for (int i = 0; i < noOfDisplays; i++)
-  {
+  for (uint8_t i = 0; i < noOfDisplays; i++) {
     pinMode(digits[i], OUTPUT);
   }
+   pinMode(joyButton_pushPin, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
-void loop(){
-  xValue = analogRead(pinX);
-  yValue = analogRead(pinY);
-  swValue = digitalRead(pinSW);
+void loop()
+{
+  if (selectedDigit == noOfDisplays)
+  {
+    //no digit is selected => blinking mode
+    xValue = analogRead(xPin);
+    if (xValue < minLimitValue && !isMoved)
+    {
+      //change digit to the left
+      if (digitToEdit > 0)
+        --digitToEdit;
+      else
+        digitToEdit = noOfDisplays - 1;
+      isMoved = 1;
+    }
 
-  if (swValue != lastSwValue){
-      if (swValue == LOW){
-        isBlocked = 1-isBlocked;
-      }
+    if (xValue > maxLimitValue&& !isMoved)
+    {
+      if (digitToEdit < noOfDisplays - 1)
+        ++digitToEdit;
+      else
+        digitToEdit = 0;
+      isMoved = 1;
+    }
+
+    if (xValue >= minLimitValue && xValue <= maxLimitValue)
+      isMoved = 0;
+
+    if (millis() - lastFlush >= flushInterval)
+    {
+      displayDigit(currentDigit, currentDigitsValues[currentDigit], false);
+      lastFlush = millis();
+      if (currentDigit == noOfDisplays - 1)
+        currentDigit = 0;
+      else
+        ++currentDigit;
+    }
   }
-  lastSwValue = swValue;
-  //y control
-  if (yValue < minValueY && joyMovedX == 0 && isBlocked == 0){
-    if (contorY == 1){
-      contorY = 4;
-    }
-    else {
-      if (contorY > 1){
-        contorY--;
+  else {
+    yValue = analogRead(yPin);
+
+    if (yValue < minLimitValue && !isMoved){
+      if (currentDigitsValues[selectedDigit] >= 1){
+        --currentDigitsValues[selectedDigit];
       }
-    }
-    joyMovedX = 1;
-  }
-  if (yValue > maxValueY && joyMovedX == 0 && isBlocked == 0){
-    if (contorY == 4){
-      contorY = 1;
-    }
-    else{
-      if (contorY < 4){
-        contorY++;
+      else{
+        currentDigitsValues[selectedDigit] = noOfDigits - 1;
       }
+      isMoved = 1;
     }
-    joyMovedX = 1;
-  }
-  if (minValueY <= yValue && yValue <= maxValueY){
-    joyMovedX = 0;
+
+    if (yValue > maxLimitValue&& !isMoved)
+    {
+      if (currentDigitsValues[selectedDigit] < noOfDigits - 1){
+        ++currentDigitsValues[selectedDigit];
+      }
+      else{
+        currentDigitsValues[selectedDigit] = 0;
+      }
+      isMoved = 1;
+    }
+
+    if (yValue >= minLimitValue && yValue <= maxLimitValue){
+      isMoved = 0;
+    }
+
+    if (millis() - lastFlush >= flushInterval){
+      if (currentDigit == selectedDigit){
+        displayDigit(currentDigit, currentDigitsValues[currentDigit], true);
+      }
+      else{
+        displayDigit(currentDigit, currentDigitsValues[currentDigit], false);
+      }
+
+      lastFlush = millis();
+      if (currentDigit + 1 == noOfDisplays)
+        currentDigit = 0;
+      else
+        ++currentDigit;
+    }
   }
 
-  //x control
-  if (xValue < minValueX && joyMovedY == 0 && isBlocked == 1){
-    if (contorX == 0){
-      contorX = 9;
-    }
-    else {
-      if (contorX > 0){
-        contorX--;
-      }
-    }
-    joyMovedY = 1;
-  }
-  if (xValue > maxValueX && joyMovedY == 0 && isBlocked == 1){
-    if (contorX == 9){
-      contorX = 0;
-    }
-    else {
-      if (contorX < 9){
-        contorX++;
-      }
-    }
-    joyMovedY = 1;
-  }
-  if (minValueX <= xValue && xValue <= maxValueX){
-    joyMovedY = 0;
-  }
+  if(millis() - joyButton_lastDebounce >= joyButton_pushDebounceInterval)
+  {
+    joyButton_lastDebounce = millis();
+    pushButtonValue = !digitalRead(joyButton_pushPin);
 
-  showDigit(hash[contorY]);
-  displayNumber(contorX, HIGH);
-  
+    if (pushButtonValue != joyButton_lastPushValue){
+      if (pushButtonValue){
+        Serial.println(pushButtonValue);
+        if (selectedDigit == noOfDisplays){
+          selectedDigit = digitToEdit;
+          digitToEdit = noOfDisplays;
+        }
+        else{
+          digitToEdit = selectedDigit;
+          selectedDigit = noOfDisplays;
+        }
+      }
+    joyButton_lastPushValue = pushButtonValue;
+    }
+  }
 }
